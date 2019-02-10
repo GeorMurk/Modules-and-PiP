b19e4a0785780e5ba4c03a31174c8a48b65e8f4b
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-10 03:15:01 +0300

    Add files via upload

---
+++ Module and pip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#!Import Functions from a python file
+
+#start your code by saying
+import #python file you want to use
+
+print("python file you want to use"."function from the python file")
+#!Drop the quotation marks
+
+
+
+
+
+#!Go Ahead and test it out!!!
